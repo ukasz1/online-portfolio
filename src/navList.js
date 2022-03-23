7c31981a89862f3c88e30c5904cb85c7b2a280e1
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-23 13:51:19 +0100

    nav elements iterated

---
+++ src/navList.js
@@ -0,0 +1,16 @@
+const navList = [
+  {
+    id: 1,
+    text: 'O mnie'
+  },
+  {
+    id: 2,
+    text: 'Projekty'
+  },
+  {
+    id: 3,
+    text: 'Osiągnięcia'
+  }
+];
+
+export default navList;
