bdde2fc96da86ff6a256138b8a6f9fec00f239a1
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-30 14:06:36 +0200

    refactoring Project component

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,4 @@
-import card1 from './images/card1.jpg';
-import card2 from './images/card2.jpg';
-import card3 from './images/card3.jpg';
-import card4 from './images/card4.jpg';
-import card5 from './images/card5.jpg';
-import card6 from './images/card6.jpg';
+import projects from './data/projects';
 
 const Projects = ({ actualTab }) => {
   return (
@@ -13,38 +8,14 @@ const Projects = ({ actualTab }) => {
         <div className='underline underline-projects'></div>
       </div>
       <section className="project-container">
-
-        <article className="projects-item">
-          <img src={card1} alt="card1" className='card-picture' />
-        </article>
-
-        <article className='projects-item'>
-          <img src={card2} alt="card2" className='card-picture' />
-        </article>
-
-        <article className='projects-item'>
-          <img src={card3} alt="card3" className='card-picture' />
-        </article>
-
-        <article className='projects-item'>
-          <img src={card4} alt="card4" className='card-picture' />
-        </article>
-
-        <article className='projects-item'>
-          <img src={card5} alt="card5" className='card-picture' />
-        </article>
-
-
-        <article className='projects-item'>
-          <img src={card6} alt="card6" className='card-picture' />
-        </article>
-
-
-
-
-
-
-
+        {projects.map(project => {
+          const { id, src, description } = project;
+          return (
+            <article key={id} className="projects-item">
+              <img src={src} alt={`Project-${id}`} className='card-picture' />
+            </article>
+          )
+        })}
       </section>
     </div>
   )
---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+import card1 from '../images/card1.jpg';
+import card2 from '../images/card2.jpg';
+import card3 from '../images/card3.jpg';
+import card4 from '../images/card4.jpg';
+import card5 from '../images/card5.jpg';
+import card6 from '../images/card6.jpg';
+
+const projects = [
+  {
+    id: 1,
+    src: card1,
+    desciption: ''
+  },
+  {
+    id: 1,
+    src: card2,
+    desciption: ''
+  },
+  {
+    id: 2,
+    src: card3,
+    desciption: ''
+  },
+  {
+    id: 3,
+    src: card4,
+    desciption: ''
+  },
+  {
+    id: 4,
+    src: card5,
+    desciption: ''
+  },
+  {
+    id: 5,
+    src: card6,
+    desciption: ''
+  }
+];
+
+export default projects;
