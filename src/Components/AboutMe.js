0e54af5b0cda792f62ac298cae289739464aafdf
ukasz1 <lukaszmitkowski53@gmail.com>
2022-04-06 12:37:56 +0200

    'about me' description added

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,10 +7,14 @@ const AboutMe = ({ actualTab }) => {
         <div className="underline underline-about"></div>
       </div>
       <article className="about-article">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat efficitur felis nec vestibulum. Nam non metus luctus, vehicula quam quis, consectetur elit. Curabitur nec leo nec mauris pretium vestibulum. Sed quis ex in sapien laoreet sodales ut id turpis. Nam purus nisi, semper et pellentesque ut, fermentum nec lacus. Nam tempor, dui vulputate placerat vulputate, velit sapien porttitor lectus, sed cursus ipsum nisi sed risus.
+        <p>My name is Łukasz Mitkowski, and I am a web designer with a technical and economic education.</p><br />
+
+        <p>I am interested in front-end development - React, JS, HTML and all that goes with it. Exploring Javascript is very interesting and exciting for me. I find that language very powerful. I like to know, how the world around me works, so I am also interested in back-end: Node, MongoDB. In the future I would like to be a full-stack developer.
         </p><br />
 
-        <p>Curabitur vel egestas lacus, quis vehicula quam. Aenean facilisis vulputate metus, fermentum bibendum justo efficitur et. Morbi hendrerit ipsum leo, ac lacinia magna lobortis et. Proin luctus lectus fermentum suscipit condimentum. Vestibulum est ante, suscipit vel dapibus sit amet, ullamcorper vitae orci. Vivamus in risus gravida, viverra nunc at, porta neque.</p>
+        <p>For last months I intensively expand my knowledge and create my own projects. In my opinion, there is no other way to be a good developer. I have also done some courses and small projects under the guidance of John Smilga on Udemy that I found extremely useful for my studying. However, making own projects is the most important for me.</p><br />
+
+        <p>Please do not hesitate to visit my <a href="https://github.com/ukasz1" target="_blank" rel="noreferrer">GitHub</a> profile. I also encourage you to see my online project and my certificates - click above. If you want to see the progress of my chess application please follow this <a href="https://www.youtube.com/watch?v=iV2be-UNH8U&ab_channel=Mulder" target="_blank" rel="noreferrer">YouTube link</a>.</p>
       </article>
     </div>
   )
